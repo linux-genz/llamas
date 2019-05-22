ed9fd32cf8ae7af182f0a96a1841d9714f47a8c8
bender <zach.bender@protonmail.com>
2019-05-22 11:47:17 -0600

    try subscribe in intervals when server not found

--- llamas/blueprints/device/blueprint.py
+++ llamas/blueprints/device/blueprint.py
@@ -3,68 +3,84 @@ import flask
 import os
 import json
 import jsonschema
+import posixpath
 import requests as HTTP_REQUESTS
 import logging
 import uuid
+import socket
+import time
+import threading
 from pdb import set_trace
 
 import flask_fat
-# from llamas.utils import sys_utils
-# from llamas.controls import deviceware
-Journal = self = flask_fat.Journal(__file__, url_prefix='/api/v1')
+
+class DeviceJournal(flask_fat.Journal):
 
 
-def subscribe_to_redfish():
-    print('Subscribing')
-    url = 'http://localhost:5000/redfish/v1/EventService'
-    data = {
-        '@odata.context': '/redfish/v1/$metadata#EventDestination.EventDestination',
-        '@odata.id': '/redfish/v1/EventService',
-        '@odata.type': '#EventService.v1_0_0.EventService',
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        self._is_pinging = False
 
-        'Id': '1',
-        'Name': 'EventSubscription1',
-        'Description': 'asdfasdfasdf',
 
-        'RegistryPrefixes' : 'ResourceAdded',
-        'RegistryVersion' : '1.0.0',
+    def on_post_register(self):
+        self.subscribe_to_redfish()
 
-        'EventTypes' : [ 'ResourceAdded' ],
-        'Destination' : 'http://localhost:1991/%s/add' % Journal.name,
 
-        'Context': 'Device add event',
-        'Protocol': 'Redfish'
-    }
-    try:
-        resp = HTTP_REQUESTS.post(url, data)
-    except Exception:
-        print('---- !!ERROR!! Failed to subscribe to redfish event! ---- ')
-        pass
+    def check_event_server(self, iteration_time):
+        if not self._is_pinging:
+            return
+        #FIXME: this called twice per iteration. IDK why. Probably something obvious.
+        t = threading.Timer(iteration_time, self.check_event_server, [iteration_time])
+        print('Trying to subscribe to event... [%s]' % (time.ctime()))
+        self.subscribe_to_redfish()
+        t.start()
 
-# subscribe_to_redfish()
+
+    def subscribe_to_redfish(self):
+        cfg = self.mainapp.config
+        port = cfg['PORT']
+        url = posixpath.join(cfg['ENDPOINTS']['base'], cfg['ENDPOINTS']['event_add'])
+        callback_endpoint = posixpath.join('http://',
+                                '%s:%s' % (socket.gethostname(), port),
+                                'api/v1',
+                                self.name,
+                                'add')
+        # data = {
+        #     '@odata.context': '/redfish/v1/$metadata#EventDestination.EventDestination',
+        #     '@odata.id': '/redfish/v1/EventService',
+        #     '@odata.type': '#EventService.v1_0_0.EventService',
+
+        #     'Id': '1',
+        #     'Name': 'EventSubscription1',
+        #     'Description': 'asdfasdfasdf',
+
+        #     'RegistryPrefixes' : 'ResourceAdded',
+        #     'RegistryVersion' : '1.0.0',
+
+        #     'EventTypes' : [ 'ResourceAdded' ],
+        #     'Destination' : 'http://localhost:1991/%s/add' % Journal.name,
+
+        #     'Context': 'Device add event',
+        #     'Protocol': 'Redfish'
+        # }
+        data = {
+            'callback' : callback_endpoint
+        }
+        try:
+            HTTP_REQUESTS.post(url, data)
+            self._is_pinging = False
+            logging.info('--- Subscribed to callback at %s' % callback_endpoint)
+        except Exception:
+            logging.error('---- !!ERROR!! Failed to subscribe to redfish event! ---- ')
+            if not self._is_pinging:
+                self._is_pinging = True
+                self.check_event_server(cfg.get('SUBSCRIBE_INTERVAL', 5))
+
+
+Journal = self = DeviceJournal(__file__, url_prefix='/api/v1')
 
 
 ''' ------------------------------- ROUTES ------------------------------- '''
-
-
-def YodelAyHeHUUID(random=True):
-    """Return a uuid.UUID object."""
-    if random:
-        return uuid.uuid4()
-
-    # Pick your favorite constructor, and refactor this routine accordingly.
-
-    this = uuid.UUID('12345678123456781234567812345678')
-    this = uuid.UUID(int=0x12345678123456781234567812345678)
-    this = uuid.UUID('urn:uuid:12345678-1234-5678-1234-567812345678')
-    this = uuid.UUID(bytes=b'\x12\x34\x56\x78' * 4)
-    this = uuid.UUID(bytes_le=b'\x78\x56\x34\x12\x34\x12\x78\x56' +
-                            b'\x12\x34\x56\x78\x12\x34\x56\x78')
-    this = uuid.UUID(
-        fields=(0x12345678, 0x1234, 0x5678, 0x12, 0x34, 0x567812345678))
-    this = uuid.UUID('{12345678-1234-5678-1234-567812345678}')
-    this = uuid.UUID('12345678-1234-5678-1234-567812345678')
-    return this
 
 
 @Journal.BP.route('/%s/add' % (Journal.name), methods=['POST'])
@@ -73,16 +89,23 @@ def add_cmp():
         Return nodes ID this API is running on.
     '''
     logging.info('%s/add route is called.' % Journal.name)
-
     response = {}
     status = 'nothing happened'
     code = 300
 
-    UUID = YodelAyHeHUUID()
-    zoo = Journal.mainapp.zookeeper
-    msg = zoo.build_msg(cmd=zoo.cfg.get('ADD'), gcid=4242, cclass=43, uuid=UUID)
+    body = flask.request.form
+    gcid = body.get('gcid', '-1')
+    cclass = body.get('cclass', '-1')
+    uuid_val = body.get('uuid', '-1')
+    uuid_val = uuid.UUID(uuid_val)
 
-    logging.info('Sending PID=%d UUID=%s' % (msg['pid'], str(UUID)))
+    zoo = Journal.mainapp.zookeeper
+    msg = zoo.build_msg(cmd=zoo.cfg.get('ADD'),
+                        gcid=int(gcid),
+                        cclass=int(cclass),
+                        uuid=uuid_val)
+
+    logging.info('Sending PID=%d UUID=%s' % (msg['pid'], str(uuid_val)))
 
     try:
         # If it works, get a packet.  If not, raise an error.
