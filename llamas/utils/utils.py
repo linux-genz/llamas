e6d3f3afacbfa9d0d252909b8188ab3ca98f3173
Jim Hull <jmhull@intelliprop.com>
2021-10-21 15:16:22 -0700

    Fix url validation
    Signed-off-by: Jim Hull <jmhull@intelliprop.com>

--- llamas/utils/utils.py
+++ llamas/utils/utils.py
@@ -1,23 +1,46 @@
 import re
 
+ul = '\u00a1-\uffff'  # unicode letters range (must not be a raw string)
+
+# IP patterns
+ipv4_re = r'(?:25[0-5]|2[0-4]\d|[0-1]?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}'
+ipv6_re = r'\[[0-9a-f:\.]+\]'
+
+# Host patterns
+hostname_re = r'[a-z' + ul + r'0-9](?:[a-z' + ul + r'0-9-]{0,61}[a-z' + ul + r'0-9])?'
+domain_re = r'(?:\.(?!-)[a-z' + ul + r'0-9-]{1,63}(?<!-))*' # domain names have max length of 63 characters
+tld_re = (
+    r'\.'                                # dot
+    r'(?!-)'                             # can't start with a dash
+    r'(?:[a-z' + ul + '-]{2,63}'         # domain label
+    r'|xn--[a-z0-9]{1,59})'              # or punycode label
+    r'(?<!-)'                            # can't end with a dash
+    r'\.?'                               # may have a trailing dot
+)
+#host_re = '(' + hostname_re + domain_re + tld_re + '|localhost)'
+# allow non-FQDN's
+host_re = '(' + hostname_re + domain_re + tld_re + '|' + hostname_re + '|localhost)'
+
+is_url_regex = re.compile(
+    r'^(?:http|ftp)s?://' # http(s):// or ftp(s)://
+    r'(?:\S+(?::\S*)?@)?'  # user:pass authentication
+    r'(?:' + ipv4_re + '|' + ipv6_re + '|' + host_re + ')' # localhost or ip
+    r'(?::\d{2,5})?'  # optional port
+    r'(?:[/?#][^\s]*)?'  # resource path
+    r'\Z', re.IGNORECASE)
+
 
 def is_valid_url(url):
     """
         Validates the URL and return True if all good, False otherwise.
 
-    Source: https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
+    Source:  (modified django url validation regex) https://stackoverflow.com/questions/827557/how-do-you-validate-a-url-with-a-regular-expression-in-python
     """
-    regex = re.compile(
-        r'^(?:http|ftp)s?://' # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-        r'localhost|' #localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-    return re.match(regex, url) is not None
+    return re.match(is_url_regex, url) is not None
 
+
+is_port_regex = re.compile(r':(\d){2}', re.IGNORECASE)
 
 def is_port_in_url(url):
-    regex = re.compile(r':(\d){2}', re.IGNORECASE)
-    return re.search(regex, url) is not None
+    return re.search(is_port_regex, url) is not None
