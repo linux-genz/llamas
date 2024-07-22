638fdfe835db2e0421c0ce1325e6c851f9ee2dee
Jim Hull <jmhull@intelliprop.com>
2024-07-22 16:09:57 -0700

    Version 0.6
    Signed-off-by: Jim Hull <jmhull@intelliprop.com>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('requirements.txt', 'r') as file_obj:
 
 setuptools.setup(
     name='llamas',
-    version='0.5',
+    version='0.6',
     license='GPLv2',
     author='Zach Volchak',
     author_email='zakhar.volchak@hpe.com',
